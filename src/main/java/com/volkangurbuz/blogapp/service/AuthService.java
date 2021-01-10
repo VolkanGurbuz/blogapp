571fd912ec73aa913973d3e6c8596c5e106e5389
volkangurbuz <volkangurbuz16@gmail.com>
2021-01-10 19:25:13 +0100

    added encode password using spring security

--- src/main/java/com/volkangurbuz/blogapp/service/AuthService.java
+++ src/main/java/com/volkangurbuz/blogapp/service/AuthService.java
@@ -4,6 +4,7 @@ import com.volkangurbuz.blogapp.model.User;
 import com.volkangurbuz.blogapp.repositories.UserRepository;
 import dto.RegisterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,17 @@ public class AuthService {
 
   @Autowired private UserRepository userRepository;
 
+  @Autowired private PasswordEncoder passwordEncoder;
+
   public void signUp(RegisterRequest registerRequest) {
     User user = new User();
     user.setUserName(registerRequest.getUserName());
     user.setEmail(registerRequest.getEmail());
-    user.setPassword(registerRequest.getPassword());
+    user.setPassword(encodePassword(registerRequest.getPassword()));
     userRepository.save(user);
+  }
+
+  private String encodePassword(String password) {
+    return passwordEncoder.encode(password);
   }
 }
